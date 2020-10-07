32eb8ac4f208c553edce0df8f2ff23de4e1b1125
Your Name <you@example.com>
2020-10-07 13:21:50 -0300

    Todo código java foi copiado

---
+++ src/CadastroCliente/CadastroCliente1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CadastroCliente;
+
+/**
+ *
+ * @author emers
+ */
+public class CadastroCliente1 {
+    public static void main(String[] args ) {
+        
+        //  Imnstanciando um objeto
+        Pessoa cliente = new Pessoa("La Ruama","12896300875", "Feminino","Rua preguicinha 32","Indaiatuba","SP");
+        
+        
+        
+    }
+}
